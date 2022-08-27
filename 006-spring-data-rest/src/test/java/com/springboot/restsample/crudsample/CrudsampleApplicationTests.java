699f3e15f9471ea346c3e36169dcd785dda77b6d
Nemat <A.Nemat@shaparak.com>
2022-08-27 10:24:19 +0430

    spring data rest sample added

---
+++ 006-spring-data-rest/src/test/java/com/springboot/restsample/crudsample/CrudsampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.restsample.crudsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudsampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
